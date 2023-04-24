6895264faef8897955d47b5c9c36094546766fdf
vaibhavbathla69 <127890471+vaibhavbathla69@users.noreply.github.com>
2023-04-24 11:13:25 +0530

    Delete src directory

--- src/core/helper/coreapicalls.js
+++
@@ -1,8 +0,0 @@
-import {API} from "../../backend"
-export const getProducts = () => {
-    return fetch(`${API}product/`, {method: 'GET'})
-    .then((Response) =>{
-        return Response.json();
-    })
-    .catch(err => console.log(err));
-};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import  Routes from "./Routes";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render((<Routes />));
